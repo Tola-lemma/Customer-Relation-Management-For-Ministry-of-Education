b164e31b55b2f5eb82025cb1046893e157f142e9
Tola-lemma <tolalemma@gmail.com>
2023-03-25 00:40:42 +0300

    refactor: project restructured

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Customer Relation Management For Mistry of Education</h1>
+    </div>
+  );
+}
+
+export default App;
