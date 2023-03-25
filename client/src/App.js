892f4e4c0557f61d240bd2cb0169da015a15fbc6
Tola-lemma <tolalemma@gmail.com>
2023-03-26 00:07:21 +0300

    feat:Navigation bar is rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import {  NavBar } from './ClientContainer/HeaderAndFooter/header/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Customer Relation Management For Mistry of Education</h1>
     </div>
   );
