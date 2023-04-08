90fe0c199b2b24128f68197898d33286a5655f6b
Tola-lemma <tolalemma@gmail.com>
2023-04-08 10:58:42 +0300

    refactor:project structure knowledgebase,supportcenterhome, ticket(open and status) is added

---
+++ client/src/ClientContainer/Home/SupportCenterHome/SupportHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SupportHome.css";
+import { NavBar } from "./../.././HeaderAndFooter/header/NavBar";
+export const SupportHome = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container">SupportHome</div>
+    </div>
+  );
+};
---
+++ client/src/ClientContainer/Home/Ticket/CheckTicketStatus/CheckTicket.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavBar } from "../../../HeaderAndFooter/header/NavBar";
+import './CheckTicket.css'
+export const CheckTicket = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container">CheckTicket</div>
+    </div>
+  );
+};
