5711b9cb3912a8195730059a1d310d3e317d21d2
Tola-lemma <tolalemma@gmail.com>
2023-04-08 18:38:19 +0300

    feat:support home page updated

--- client/src/ClientContainer/Home/SupportCenterHome/SupportHome.js
+++ client/src/ClientContainer/Home/SupportCenterHome/SupportHome.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "./SupportHome.css";
 import { NavBar } from "./../.././HeaderAndFooter/header/NavBar";
+import { Link } from "react-router-dom";
 export const SupportHome = () => {
   return (
     <div>
       <NavBar />
-      <div className="container">SupportHome</div>
+      <div className="container SupportHomeContainer">
+        <div className="landing-page">
+            <div className="main-content">
+               <div className="search-box">
+                 <input type="text" className="form-control   " placeholder="search our knowledgebase"/>
+                 <button className="btn btn-primary rounded-pill"> Search </button>
+               </div>
+              <div className="welcome_message">
+                   <h1>Welcome to the MoE Support Center</h1>
+               </div>
+            </div>
+            {/* <div className="sidebar"> */}
+                <div className="sidebar-container">
+                    <div className="front-button">
+                        <p><Link to="/opennewticket"  className="btn btn-primary rounded-pill open">Open a New Ticket</Link></p>
+                        <p><Link to="/checkticketstatus" className="btn btn-warning rounded-pill check">Check Ticket Status</Link></p>
+                    </div>
+               <div className="featured-questions">
+                   <section>
+                     <div className="first-text">
+                       Featured Questions 
+                     </div>
+                   </section>
+              </div>
+            </div>
+           {/* </div> */}
+        </div>
+    </div>
     </div>
   );
 };
