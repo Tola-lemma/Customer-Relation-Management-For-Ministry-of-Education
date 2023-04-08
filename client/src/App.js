a9e368bd39ac754208dcabed18d5b4cd589e7318
Tola-lemma <tolalemma@gmail.com>
2023-04-08 10:44:06 +0300

    refactor: router is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { SupportHome } from "./ClientContainer/Home/SupportCenterHome/SupportHome";
 import { Knowledgebase } from "./ClientContainer/Home/Knowledgebase/Knowledgebase";
+import { OpenTicket } from "./ClientContainer/Home/Ticket/OpenNewTicket/OpenTicket";
+import { CheckTicket } from "./ClientContainer/Home/Ticket/CheckTicketStatus/CheckTicket";
 export const App =()=> {
   const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const App =()=> {
     {
       path: "/knowledgebase",
       element: <Knowledgebase />,
+    },
+    {
+      path: "/opennewticket",
+      element: <OpenTicket />,
+    },
+    {
+      path: "/checkticketstatus",
+      element: <CheckTicket/>,
     }
 ])
   return <RouterProvider router={router}/>
