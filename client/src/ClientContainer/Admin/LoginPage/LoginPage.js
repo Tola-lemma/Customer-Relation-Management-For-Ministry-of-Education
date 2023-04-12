5b87e885947889a6316096f3859c7327d8b84f44
Tola-lemma <tolalemma@gmail.com>
2023-04-12 23:12:25 +0300

    feat:Login page and admin page is initialized

---
+++ client/src/ClientContainer/Admin/LoginPage/LoginPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+export const LoginPage = () => {
+  return (
+    <div className="loginpage">
+      <div className="login-box">
+        <div className="login-parent">
+          <h2>Login</h2>
+          <form>
+            <div className="input-box">
+              <span className="icon">
+                <i className="fa-solid fa-envelope"></i>
+              </span>
+              <input type="email" required />
+              <label htmlFor="email"> Email</label>
+            </div>
+            <div className="input-box">
+              <span className="icon">
+                <i className="fa-solid fa-lock"></i>
+              </span>
+              <input type="password" required />
+              <label htmlFor="password"> Password</label>
+            </div>
+            <div className="forgotPassword">
+              <Link to="#" className="forgotpasswordLink">Forgot Password?</Link>
+            </div>
+            <button type="submit" className="loginbtn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
