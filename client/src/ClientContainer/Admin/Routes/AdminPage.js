8a1d72554923da29f75fd86126ba56941a333602
Tola-lemma <tolalemma@gmail.com>
2023-04-14 07:48:15 +0300

    feat:Topbar is rendered

--- client/src/ClientContainer/Admin/Routes/AdminPage.js
+++ client/src/ClientContainer/Admin/Routes/AdminPage.js
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import  {CssBaseline,ThemeProvider} from "@mui/material"
+import { ColorModeContext,useMode } from '../theme'
+import { Topbar } from "../Pages/global/Topbar";
 export const AdminPage = () => {
+  const [theme,colorMode] = useMode();
   return (
-    <div>AdminPage</div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
