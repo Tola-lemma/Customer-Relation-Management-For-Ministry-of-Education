1bbb1771f2f4630eb20ef79c004a5cb853075883
zbluee <amanueltemesgen9@gmail.com>
2023-04-25 15:27:00 -0500

    feat: Add custom error classes
    This commit adds custom error classes for common HTTP errors: BadRequestError, NotFoundError, and UnauthorizedError. These classes extend a base class, CustomApiError, which allows for consistent error handling throughout the application.

---
+++ server/error/errors.js
@@ -0,0 +1,30 @@
+import { StatusCodes } from "http-status-codes";
+
+class CustomApiError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomApiError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotfoundError extends CustomApiError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
+class UnauthorizedError extends CustomApiError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+export {CustomApiError, BadRequestError, NotfoundError, UnauthorizedError}
