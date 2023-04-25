44ff446f45e0cdb41963ce9e67ddac2968647465
zbluee <amanueltemesgen9@gmail.com>
2023-04-25 15:54:47 -0500

    feat: add authroute and error handling middlewares This commit adds the auth router to the server, so that the endpoints for the auth functionality can be accessed via the  route.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 import express from "express";
+import { routeNotFoundMiddleware } from "./middleware/notfound.js";
+import { errorHandlerMiddleware } from "./middleware/errorHandler.js";
+import { connectDatabase } from "./db/connection.js";
+import {authRouter} from "./routes/auth.js"
 
 const server = express();
 const port = 3000 || process.env.PORT;
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).json({ msg: "home page" });
-});
+server.use('/api/v1/auth', authRouter)
 
-server.listen(port, () => console.log(`server is listening on port ${port} ...`));
+// Add the error handling middlewares
+server.use(errorHandlerMiddleware);
+server.use(routeNotFoundMiddleware);
+
+const start = async () => {
+  try {
+    await connectDatabase(process.env.MONGO_CONNECTION_STRING);
+    server.listen(port, () =>
+      console.log(`server is listening on port ${port} ...`)
+    );
+  } catch (error) {}
+};
+
+start();
