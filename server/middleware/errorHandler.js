c296fe3e86356342519a5282e8111ae5f6be8116
zbluee <amanueltemesgen9@gmail.com>
2023-05-02 05:15:31 -0500

    feat: Improve error handling for MongoDB errors
    - Enabled the application to handle MongoDB errors such as duplicates, casting, and validation errors.
    - Improved the error handling to provide more specific error messages for MongoDB errors.

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,11 +2,33 @@ import { StatusCodes } from "http-status-codes";
 
 export const errorHandlerMiddleware = async (err, req, res, next) => {
   let customError = {
-    message: err.message || "something went wrong. please try again lator",
+    message: err.message || "something went wrong. please try again later",
     statusCode: err.statuscode || StatusCodes.INTERNAL_SERVER_ERROR,
   };
+  //mongo errors
+  //duplicate error for username
+  if (err.code == 11000) {
+    customError.message = `duplicate values for ${Object.keys(
+      err.keyValue
+    )}, please choose another value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
 
-  res
+  //casting error
+  if (err.name === "CastError") {
+    customError.message = `No item with id : ${err.value} found.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  //validation error
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res
     .status(customError.statusCode)
     .json({ success: false, msg: customError.message });
 };
