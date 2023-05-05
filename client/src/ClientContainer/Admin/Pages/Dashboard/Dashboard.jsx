0b6c8894debdea96ad6cb0fd5cbbf8c0bc367862
Tola-lemma <tolalemma@gmail.com>
2023-05-05 10:56:37 +0300

    feat:dashboard updated with important element of dashboard

--- client/src/ClientContainer/Admin/Pages/Dashboard/Dashboard.jsx
+++ client/src/ClientContainer/Admin/Pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Header } from '../../components/Header'
-import { Box, Button, useTheme } from '@mui/material'
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material'
 import { tokens } from '../../theme';
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import StatBox from "../../components/StatBox";
 
 export const Dashboard = () => {
   const theme = useTheme();
@@ -29,6 +34,185 @@ export const Dashboard = () => {
           </Button>
         </Box>
       </Box>
+            {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="5"
+            subtitle="Number of staff assigned"
+            progress="0.85"
+            increase="+90%"
+            icon={
+              <PointOfSaleIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="441"
+            subtitle="New Customer"
+            progress="0.30"
+            increase="+5%"
+            icon={
+              <PersonAddIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="441"
+            subtitle="Issues Received"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2  for specific report*/}
+        <Box
+          gridColumn="span 12"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h3"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Download Specific report
+                <hr/>
+              </Typography>
+            </Box>
+            <Box display="flex " justifyContent="space-between">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color={colors.greenAccent[200]}
+              >
+                Student Transfer
+              </Typography>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+            <Box display="flex " justifyContent="space-between">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color={colors.greenAccent[200]}
+              >
+                Teacher Transfer
+              </Typography>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+            <Box display="flex " justifyContent="space-between">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color={colors.greenAccent[200]}
+              >
+                Scholarship Question
+              </Typography>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+            <Box display="flex " justifyContent="space-between">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color={colors.greenAccent[200]}
+              >
+                A request to return to work after completing their studies abroad
+              </Typography>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+            <Box display="flex " justifyContent="space-between">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color={colors.greenAccent[200]}
+              >
+                Various academic and administrative complaints
+              </Typography>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          </Box>
+          </Box>
     </Box>
   )
 }
