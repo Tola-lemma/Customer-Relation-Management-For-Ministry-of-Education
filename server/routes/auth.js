9efb06a923c2e58722ac52c94dcde4ebfe541899
zbluee <amanueltemesgen9@gmail.com>
2023-05-07 15:38:14 -0500

    feat: add forget and reset password routes
    This commit adds three new routes to handle password reset functionality: '/forget-password', '/reset-password/:token', and '/reset-password/:token/:userId'. The '/forget-password' endpoint triggers the 'forgetPassword' controller function to send an email to the user with a link to reset their password. The '/reset-password/:token' endpoint triggers the 'resetPassword' controller function to update the user's password using the provided token. The '/reset-password/:token/:userId' endpoint triggers the 'getResetPassword' controller function to prompt the user to enter their new password.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login, changePassword } from "../controllers/auth.js";
+import { login, changePassword, forgetPassword, resetPassword, getResetPassword } from "../controllers/auth.js";
 import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/change-password", authMiddleware, changePassword);
+router.post("/forget-password", forgetPassword)
+router.post("/reset-password/:token", resetPassword)
+router.get("/reset-password/:token/:userId", getResetPassword)
 
 export { router as authRoute };
