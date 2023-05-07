a1b2799b93b3b613aba3be9ac17c28010724b9e6
zbluee <amanueltemesgen9@gmail.com>
2023-05-07 17:27:30 -0500

    feat: add rate limiting middleware
    This commit adds rate limiting middleware to the application using the express-slow-down package. Three separate speed limiters are defined: loginSpeedLimiter with a limit of 5 requests every 10 minutes and a 100ms delay after the 5th request, resetPasswordSpeedLimiter with a limit of 3 requests every 10 minutes and a 100ms delay after the 3rd request, and speedLimiter with a limit of 100 requests every 15 minutes and a 500ms delay per request above 100.

--- server/utils/rateLimiters.js
+++ server/utils/rateLimiters.js
@@ -1,4 +1,5 @@
 import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
 
 export const loginLmiter = rateLimit({
   windowMs: 10 * 60 * 1000, //10 minutes
@@ -25,4 +26,22 @@ export const rateLimiter = rateLimit({
   max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+export const loginSpeedLimiter = slowDown({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  delayAfter: 5, // allow 5 requests to go at full-speed, then...
+  delayMs: 100, // 5th request has a 100ms delay...
+});
+
+export const resetPasswordSpeedLimiter = slowDown({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  delayAfter: 3, // allow 3 requests to go at full-speed, then...
+  delayMs: 100, // 4th request has a 100ms delay...
+});
+
+export const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 200, // allow 100 requests per 15 minutes, then...
+  delayMs: 500, // begin adding 500ms of delay per request above 100:
 });
