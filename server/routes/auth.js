e0583605790d36216946d497fa893b5ab918abd7
zbluee <amanueltemesgen9@gmail.com>
2023-05-07 17:29:56 -0500

    feat: add rate limiting to login and reset password routes
    This commit adds rate limiting to the '/login' and '/reset-password/:token' endpoints.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { login, changePassword, forgetPassword, resetPassword, getResetPassword } from "../controllers/auth.js";
 import { authMiddleware } from "../middleware/auth.js";
+import {loginLmiter, loginSpeedLimiter, resetPasswordLimiter, resetPasswordSpeedLimiter } from "../utils/rateLimiters.js"
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", loginLmiter, loginSpeedLimiter, login);
 router.post("/change-password", authMiddleware, changePassword);
 router.post("/forget-password", forgetPassword)
-router.post("/reset-password/:token", resetPassword)
+router.post("/reset-password/:token", resetPasswordLimiter, resetPasswordSpeedLimiter, resetPassword)
 router.get("/reset-password/:token/:userId", getResetPassword)
 
 export { router as authRoute };
