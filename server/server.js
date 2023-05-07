5c2c88f1974fd562ca926ab40d34d6cc7a481f56
zbluee <amanueltemesgen9@gmail.com>
2023-05-07 17:33:19 -0500

    feat: add rate and speed limiting middleware
    This commit updates the Express server to include two new middleware functions: rateLimiter and speedLimiter. These functions help to mitigate against abusive traffic and prevent server overload by limiting the number of requests per IP address and the rate at which requests are processed.

--- server/server.js
+++ server/server.js
@@ -8,12 +8,13 @@ import { authRoute } from "./routes/auth.js";
 import { adminRoute } from "./routes/admin.js";
 import { authMiddleware, isAuthorized } from "./middleware/auth.js";
 import { Roles } from "./models/roles.js";
+import { rateLimiter, speedLimiter} from "./utils/rateLimiters.js"
 
 const server = express();
 const port = 3000 || process.env.PORT;
 
 server.use(express.json());
-
+server.use(rateLimiter, speedLimiter);
 server.use("/api/v1/auth", authRoute);
 server.use(
   "/api/v1/admin",
