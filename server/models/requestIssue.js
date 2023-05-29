7a9c0ced07ad34ddc22030b5af5448171d742e96
zbluee <amanueltemesgen9@gmail.com>
2023-05-29 12:52:46 -0500

    feat: Add RequestIssue model with fields
    Added RequestIssue model to represent user-submitted issues.
    Included fields: name, email, phoneNumber, serviceType, issueDescription, and issueStatus.

---
+++ server/models/requestIssue.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const requestIssueSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required."],
+      minLength: 5,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required."],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "invalid email address",
+      ],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "phone number is required."],
+      minLength: 10,
+    },
+    serviceType: {
+      type: String,
+      required: [true, "service type is required."],
+      enum: [
+        "teacherStudentTransfer",
+        "studyAbroad",
+        "scholarship",
+        "complaint",
+      ],
+    },
+    issueDescription: {
+      type: String,
+      required: [true, "issue description or summary is required"],
+      trim : true,
+      minLength: 10,
+    },
+    issueStatus: {
+      type: String,
+      required: true,
+      enum: ["todo", "inprogress", "done"],
+      default: "todo",
+    },
+  },
+  { timestamps  : true}
+);
+
+export const RequestIssue = mongoose.model("RequestIssue", requestIssueSchema);
