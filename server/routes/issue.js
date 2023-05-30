14ae135dc5fdd40e46040d7105d58b82232474a5
zbluee <amanueltemesgen9@gmail.com>
2023-05-30 08:00:14 -0500

    feat: add /requested-issues get route for the getIssues controller and add auth and role based auth middleware.

--- server/routes/issue.js
+++ server/routes/issue.js
@@ -1,8 +1,10 @@
 import express from "express";
 import multer from "multer";
-import { upload, trackIssue } from "../controllers/issue.js";
+import { upload, trackIssue, getIssues } from "../controllers/issue.js";
 import {storage} from "../utils/storage.js"
 import {fileFilter, limits} from "../utils/fileFilter.js"
+import { authMiddleware, isAuthorized } from "../middleware/auth.js";
+import { Roles } from "../models/roles.js";
 
 const router = express.Router();
 
@@ -10,5 +12,6 @@ const uploadM = multer({storage, fileFilter, limits})
 
 router.post("/ticket-issue",  uploadM.array("files", limits.files), upload);
 router.get("/track-issue", trackIssue);
+router.get("/requested-issues", authMiddleware, isAuthorized(Roles.ComplaintsCoordinator, Roles.ScholarshipCoordinator, Roles.StudyAbroadCoordinator, Roles.TransferCoordinator), getIssues)
 
 export { router as requestIssueRouter };
