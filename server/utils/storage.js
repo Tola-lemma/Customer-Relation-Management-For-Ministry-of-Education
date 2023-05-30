a1e9bdc135c184c8ed58161d72e9ee77088e4b3f
zbluee <amanueltemesgen9@gmail.com>
2023-05-30 07:53:06 -0500

    refactor: improve storage function behavior and error handling
    This commit enhances the storage function by making the following improvements:
    
    Consolidates multiple file submissions by a user into a single requestIssue, regardless of the number of files.
    Addresses an issue where errors during issue creation bypassed the global error handler due to the use of async functions within promises.

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -10,30 +10,32 @@ export const storage = new GridFsStorage({
     return new Promise((resolve, reject) => {
       crypto.randomBytes(16, async (err, buf) => {
         if (err) return reject(new BadRequestError(err.message));
-        await createIssue(req);
-        
-        return resolve({
-          filename: buf.toString("hex") + path.extname(file.originalname), // modify the file name using random hex string inorger to prevent file duplicate
-          bucketName: "files", // Name of the MongoDB collection,
-          metadata: {
-            requestIssueId: req.requestIssue.Id,
-            originalname: file.originalname,
-          }, ////add requestIssue as metadata
-        });
+        try {
+          if (!req.requestIssue) {
+            // If requestIssueId doesn't exist, create a new RequestIssue
+            await createIssue(req);
+          }
+          return resolve({
+            filename: buf.toString("hex") + path.extname(file.originalname), // modify the file name using random hex string inorger to prevent file duplicate
+            bucketName: "files", // Name of the MongoDB collection,
+            metadata: {
+              requestIssueId: req.requestIssue.Id, //add requestIssueId as metadata
+              originalname: file.originalname,
+            },
+          });
+        } catch (error) {
+          reject(error);
+        }
       });
     });
   },
 });
 
 const createIssue = async (req) => {
-  try {
-    if (!req.body.name || !req.body.email || !req.body.issueDescription)
-      throw new BadRequestError(
-        "please provide name, email and issue descritpion for the requested isssue."
-      );
-    const reqestIssue = await RequestIssue.create({ ...req.body });
-    req.requestIssue = { Id: reqestIssue._id };
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  if (!req.body.name || !req.body.email || !req.body.issueDescription)
+    throw new BadRequestError(
+      "please provide name, email and issue descritpion for the requested isssue."
+    );
+  const requestIssue = await RequestIssue.create({ ...req.body });
+  req.requestIssue = { Id: requestIssue._id };
 };
