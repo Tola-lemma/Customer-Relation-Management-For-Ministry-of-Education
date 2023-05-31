0dd5209b433cc24bc6a20c3e8f2e7cefe8f17bcc
zbluee <amanueltemesgen9@gmail.com>
2023-05-31 12:50:48 -0500

    feat: add get route for getFile function of issue controller.

--- server/routes/issue.js
+++ server/routes/issue.js
@@ -1,17 +1,43 @@
 import express from "express";
 import multer from "multer";
-import { upload, trackIssue, getIssues } from "../controllers/issue.js";
-import {storage} from "../utils/storage.js"
-import {fileFilter, limits} from "../utils/fileFilter.js"
+import {
+  upload,
+  trackIssue,
+  getIssues,
+  getFile,
+} from "../controllers/issue.js";
+import { storage } from "../utils/storage.js";
+import { fileFilter, limits } from "../utils/fileFilter.js";
 import { authMiddleware, isAuthorized } from "../middleware/auth.js";
 import { Roles } from "../models/roles.js";
 
 const router = express.Router();
 
-const uploadM = multer({storage, fileFilter, limits})
+const uploadM = multer({ storage, fileFilter, limits });
 
-router.post("/ticket-issue",  uploadM.array("files", limits.files), upload);
+router.post("/ticket-issue", uploadM.array("files", limits.files), upload);
 router.get("/track-issue", trackIssue);
-router.get("/requested-issues", authMiddleware, isAuthorized(Roles.ComplaintsCoordinator, Roles.ScholarshipCoordinator, Roles.StudyAbroadCoordinator, Roles.TransferCoordinator), getIssues)
+router.get(
+  "/requested-issues",
+  authMiddleware,
+  isAuthorized(
+    Roles.ComplaintsCoordinator,
+    Roles.ScholarshipCoordinator,
+    Roles.StudyAbroadCoordinator,
+    Roles.TransferCoordinator
+  ),
+  getIssues
+);
+router.get(
+  "/file/:filename",
+  authMiddleware,
+  isAuthorized(
+    Roles.ComplaintsCoordinator,
+    Roles.ScholarshipCoordinator,
+    Roles.StudyAbroadCoordinator,
+    Roles.TransferCoordinator
+  ),
+  getFile
+);
 
 export { router as requestIssueRouter };
