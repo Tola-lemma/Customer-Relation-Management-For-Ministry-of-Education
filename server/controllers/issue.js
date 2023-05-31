05961f027de6d13baf3866648d48c9ca9d64bb3a
zbluee <amanueltemesgen9@gmail.com>
2023-05-31 15:03:45 -0500

    feat: Add getRequestedIssue function to issue controller.

--- server/controllers/issue.js
+++ server/controllers/issue.js
@@ -2,107 +2,158 @@ import { StatusCodes } from "http-status-codes";
 import { RequestIssue } from "../models/requestIssue.js";
 import { BadRequestError, NotfoundError } from "../error/errors.js";
 import { sendMail } from "../utils/sendmail.js";
-import { requestDoneNotificationMailOptions, requestNotificationMailOptions } from "../utils/mailOptions.js";
+import {
+  requestDoneNotificationMailOptions,
+  requestNotificationMailOptions,
+} from "../utils/mailOptions.js";
 import { ServiceTypes } from "../models/serviceTypes.js";
 import mongoose from "mongoose";
 import { IssueStatus } from "../models/issueStatus.js";
 
 export const upload = async (req, res) => {
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      success: true,
-      msg: "file uploaded successfully, please check your email to get your ticket number for the submitted issue.",
-    });
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    msg: "file uploaded successfully, please check your email to get your ticket number for the submitted issue.",
+  });
 
   const requestIssue = await RequestIssue.findById(req.requestIssue.Id);
 
   if (!requestIssue)
     throw new NotfoundError(`No request issue with ${req.requestIssue.Id}.`);
-  
-  await sendMail(requestNotificationMailOptions(requestIssue.name, requestIssue.email,requestIssue._id));
+
+  await sendMail(
+    requestNotificationMailOptions(
+      requestIssue.name,
+      requestIssue.email,
+      requestIssue._id
+    )
+  );
 };
 
 export const trackIssue = async (req, res) => {
   const { ticket } = req.query;
   const requestedIssue = await RequestIssue.findById(ticket);
   if (!requestedIssue)
-    throw new NotfoundError(`No issue is request using with ticket : ${ticket}`);
+    throw new NotfoundError(
+      `No issue is request using with ticket : ${ticket}`
+    );
 
-  res.status(StatusCodes.OK).json({ success: true, status: requestedIssue.status, requestedIssue });
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, status: requestedIssue.status, requestedIssue });
 };
 
 export const getIssues = async (req, res) => {
-  const {role} = req.user;
-  const serviceType = ServiceTypes[role]
-  const requestIssue = await RequestIssue.find({serviceType})
-  // Create a new GridFSBucket instance 
+  const { role } = req.user;
+  const serviceType = ServiceTypes[role];
+  const requestIssue = await RequestIssue.find({ serviceType });
+  // Create a new GridFSBucket instance
   const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName : "files"
-  })
-  const requestIssueIds = Object.values(requestIssue).map((requestIssue) => requestIssue._id)
-  const file = await bucket.find({"metadata.requestIssueId" : {$in : requestIssueIds }}).toArray()
+    bucketName: "files",
+  });
+  const requestIssueIds = Object.values(requestIssue).map(
+    (requestIssue) => requestIssue._id
+  );
+  const file = await bucket
+    .find({ "metadata.requestIssueId": { $in: requestIssueIds } })
+    .toArray();
 
-  res.status(StatusCodes.OK).json({count : requestIssue.length, serviceType, requestIssue, file})  
-}
+  res
+    .status(StatusCodes.OK)
+    .json({ count: requestIssue.length, serviceType, requestIssue, file });
+};
 
-export const getFile = async(req, res) => {
-  const {filename} = req.params
+export const getRequestedIssue = async (req, res) => {
+  const { requestId } = req.params;
+  const serviceType = ServiceTypes[req.user.role];
 
-  if(!filename) throw new BadRequestError("file is required.")
+  const requestedIssue = await RequestIssue.findOne({
+    _id: requestId,
+    serviceType: serviceType,
+  });
+  if (!requestedIssue) throw new NotfoundError(`No requestIssue found.`);
 
-  // Create a new GridFSBucket instance 
+  res.status(StatusCodes.OK).json({ success: true, requestedIssue });
+};
+
+export const getFile = async (req, res) => {
+  const { filename } = req.params;
+
+  if (!filename) throw new BadRequestError("file is required.");
+
+  // Create a new GridFSBucket instance
   const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName : "files"
-  })
+    bucketName: "files",
+  });
 
   // find a file that with filename from the bucket
-  const file = await bucket.find({filename}).toArray();
+  const file = await bucket.find({ filename }).toArray();
 
-  if(file.length == 0) throw new NotfoundError(`No file with : ${filename} exist`);
+  if (file.length == 0)
+    throw new NotfoundError(`No file with : ${filename} exist`);
 
-  res.status(StatusCodes.OK).json({success : true, file})
-}
+  res.status(StatusCodes.OK).json({ success: true, file });
+};
 
-export const streamFile = async(req, res) => {
-  const { filename } = req.params
-  if(!filename) throw new BadRequestError("file is required.")
+export const streamFile = async (req, res) => {
+  const { filename } = req.params;
+  if (!filename) throw new BadRequestError("file is required.");
 
-  // Create a new GridFSBucket instance 
+  // Create a new GridFSBucket instance
   const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName : "files"
-  })
+    bucketName: "files",
+  });
   // find a file that with filename from the bucket
-  const downloadStream = bucket.openDownloadStreamByName(filename)
+  const downloadStream = bucket.openDownloadStreamByName(filename);
   // Pipe the download stream to the response object to send the file
   downloadStream.pipe(res);
-}
+};
 
-export const updateIssueStatus = async(req, res) => {
-  const {status} = req.body;
-  const {filename} = req.params;
-  const serviceType = ServiceTypes[req.user.role]
+export const updateIssueStatus = async (req, res) => {
+  const { status } = req.body;
+  const { filename } = req.params;
+  const serviceType = ServiceTypes[req.user.role];
 
-  if(!filename || !status) throw new BadRequestError("please provide a complete information, filename and status is requred.")
-  
-  if(!IssueStatus[status]) throw new BadRequestError("Invalid value for issue status.")
+  if (!filename || !status)
+    throw new BadRequestError(
+      "please provide a complete information, filename and status is requred."
+    );
+
+  if (!IssueStatus[status])
+    throw new BadRequestError("Invalid value for issue status.");
 
   const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName : "files"
-  })
+    bucketName: "files",
+  });
 
-  const issue = await bucket.find({filename}).toArray();
-  if(!filename) throw new BadRequestError(`No file name with ${filename} exist.`)
+  const issue = await bucket.find({ filename }).toArray();
+  if (!filename)
+    throw new BadRequestError(`No file name with ${filename} exist.`);
 
   const requestId = issue[0].metadata.requestIssueId;
-  const requestedIssue = await RequestIssue.findOneAndUpdate({_id : requestId, serviceType : serviceType}, { issueStatus : IssueStatus[status]}, {runValidators : true, new : true})
-  
-  if(!requestedIssue) throw new NotfoundError(`No requestIssue found.`)
+  const requestedIssue = await RequestIssue.findOneAndUpdate(
+    { _id: requestId, serviceType: serviceType },
+    { issueStatus: IssueStatus[status] },
+    { runValidators: true, new: true }
+  );
 
-  res.status(StatusCodes.OK).json({success : true, msg : "issue status updated successfully.", requestedIssue})
+  if (!requestedIssue) throw new NotfoundError(`No requestIssue found.`);
 
-  if(requestedIssue.issueStatus === IssueStatus.done) {
-    await sendMail(requestDoneNotificationMailOptions(requestedIssue.name, "savix11466@soremap.com", requestId))
+  res
+    .status(StatusCodes.OK)
+    .json({
+      success: true,
+      msg: "issue status updated successfully.",
+      requestedIssue,
+    });
+
+  if (requestedIssue.issueStatus === IssueStatus.done) {
+    await sendMail(
+      requestDoneNotificationMailOptions(
+        requestedIssue.name,
+        "savix11466@soremap.com",
+        requestId
+      )
+    );
   }
-}
-
+};
