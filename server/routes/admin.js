713df308076424c2cdfa47e113439861f2899245
zbluee <amanueltemesgen9@gmail.com>
2023-06-21 15:30:19 -0500

    feat: add post route endpoints for update account and changePassword.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,6 +7,7 @@ import {
   updateRole,
 } from "../controllers/admin.js";
 import { generateReport, getIssues } from "../controllers/issue.js";
+import { changePassword, updateAccount } from "../controllers/auth.js";
 
 const router = express.Router();
 
@@ -17,5 +18,7 @@ router.post("/update-role/:id", updateRole);
 router.delete("/delete-account/:id", removeAccount);
 router.get("/requested-issues",getIssues);
 router.get("/report", generateReport)
+router.post("/update-account", updateAccount)
+router.post("/change-password", changePassword)
 
 export { router as adminRoute };
